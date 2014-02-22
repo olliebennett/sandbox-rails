c9f1fbacf38a2247bba03eae8736867bd6ede752
Ollie Bennett <olliebennett@gmail.com>
2014-02-22 22:06:36 +0000

    Creating a new demo app.
    See http://ruby.railstutorial.org/chapters/a-demo-app

---
+++ rails_projects/demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = 'eb17f292198d33bb2d10ecd59c49c64318d3feb5eb62e859f6f71c1b3d28ed0a5d39abf0c8fc9624ab9f3475a50e0adc157cf3a228e8505589a7ea8425455958'
