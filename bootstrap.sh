a864d926ba5ee93638afe24a184a40c621ff5b19
Ollie Bennett <olliebennett@gmail.com>
2014-02-22 19:54:51 +0000

    Tweaks to dependencies and provisioning
    resolving an issue with missing JavaScript runtime.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,29 +1,22 @@
 #!/usr/bin/env bash
 
 # Update
-#apt-get update
+apt-get update
 
 # Git
 apt-get install -y git
 
-# Install rbenv (Ruby Environment)
-# git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
-# echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-# echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
-# source ~/.bashrc
-
 # Install correct version of Ruby (default 1.8.7 does not work)
 apt-get install -y ruby1.9.3
 
-#update-alternatives --set ruby /usr/bin/ruby1.9.1
-
-#gem install bundler
-
 # Install Rails dependencies
-# (or we get make: command not found error).
+# ... avoid 'make: command not found' error.
 apt-get install -y build-essential
-
+# ... avoid issues when running 'bundle install'
 apt-get install -y libxslt-dev libxml2-dev libsqlite3-dev
 
 # Install Ruby on Rails!
 gem install rails
+
+# Install ExecJS (required to find JavaScript runtime)
+gem install execjs
