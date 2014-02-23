8e0b5d408a714db1d777247b2e2e26182a7c8545
Ollie Bennett <olliebennett@gmail.com>
2014-02-23 10:29:06 +0000

    Create new sample_app.
    See: http://ruby.railstutorial.org/chapters/static-pages

---
+++ rails_projects/sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = 'fd2240488f9dfea4e428e89ca25bc6747ebf29ccca4834f8b1ecaaa4c01c67a94ecba3dfc0ab8ac1d9711fe3fd309cd1f73e63956b927d490c1ada9ade6c987b'
